51f0076800b0692fb332793b1ebccd85d243dee3
Sakiskid <tylerdrew.home@gmail.com>
2020-07-28 14:10:59 -0500

    more work on responsiveness, good progress. about to remove Tyler Smith header and place it into navbar, so committing before doing that

---
+++ imgCollapser.js
@@ -0,0 +1,19 @@
+let bioImg = document.getElementById("bio-picture-img");
+let bioImgSmall = document.getElementById("bio-picture-img--sm");
+
+function imgMediaQuery(xWindowSize) {
+    if (xWindowSize.matches) {
+        console.log("Showing Great Than Medium Image!");
+        bioImg.style.visibility = "visible";
+        bioImgSmall.style.visibility = "hidden";
+    } else {
+        console.log("Shower Small and Lower Image!");
+        bioImgSmall.style.visibility = "visible";
+        bioImg.style.visibility = "hidden";
+    }
+}
+
+
+let xWindowSize = window.matchMedia("(min-width: 992px)")
+imgMediaQuery(xWindowSize);
+xWindowSize.addListener(imgMediaQuery);
