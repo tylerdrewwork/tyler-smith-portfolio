4a6e4b7632d6ce62825ece0e7a93da21bac162f7
Sakiskid <tylerdrew.home@gmail.com>
2020-12-06 14:24:25 -0600

    header has hover styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,24 +3,36 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 // import './header.css';
 
-const HeaderStyled = styled.header`
-    width: 100vw;
-    height: 5vh;
-    text-align: center;
-    background-color: ${props => props.theme.primarylight};
-    box-shadow: 0px 2px 2px ${props => props.theme.black};
-`;
-
 const LinkStyleFunc = ({ className, children, to }) => (
     <Link className={className} to={to}>
         {children}
     </Link>
 );
 
+const HeaderStyled = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 5vh;
+    text-align: center;
+
+    background-color: ${props => props.theme.primarylight};
+    box-shadow: 0px 2px 2px ${props => props.theme.black};
+`;
+
 const LinkStyled = styled(LinkStyleFunc)`
+    height: 100%;
+    vertical-align: middle;
+    padding: 0px 20px;
+    font-size: 1.5em;
+    text-decoration: none;
+
+    /* Style */
     color: white;
     :hover {
-        color: red;
+        color: ${props => props.theme.primaryLight};
+        border-bottom: 3px solid ${props => props.theme.primaryLight};
     }
 `;
 
