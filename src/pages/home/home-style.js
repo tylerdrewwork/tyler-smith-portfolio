c4a7fa58e17a115fd3420a49ef0a7375ce0ac649
Sakiskid <tylerdrew.home@gmail.com>
2020-12-07 14:50:32 -0600

    simplified responsiveTextFormula using es5

--- src/pages/home/home-style.js
+++ src/pages/home/home-style.js
@@ -1,25 +1,28 @@
 import { css } from 'styled-components';
 
-const headingSize = {
-    sizeMin : "20",
-    sizeMax : "100",
-    viewportMin : "320",
+const h1 = {
+    sizeMin : "20", // size min
+    sizeMax : "100", // size max
+    viewportMin : "320", // viewport
     viewportMax : "1000",
-    formula () {
-        return `${this.sizeMin}px + (${this.sizeMax} - ${this.sizeMin}) * ((100vw - ${this.viewportMin}px) / (${this.viewportMax} - ${this.viewportMin}))`
-    }
+}
+
+
+function responsiveTextFormula (properties) {
+    let {sizeMin, sizeMax, viewportMin, viewportMax} = properties;
+    return `${sizeMin}px + (${sizeMax} - ${sizeMin}) * ((100vw - ${viewportMin}px) / (${viewportMax} - ${viewportMin}))`
 }
 
 export const HeadingNameStyle = css`
     /* Font-Size: Minimum */
-    font-size: ${headingSize.sizeMin}px;
+    font-size: ${h1.sizeMin}px;
     /* Font-Size: Medium */
-    @media screen and (min-width: ${headingSize.viewportMin}px) {
-        font-size: calc(${headingSize.formula()});
+    @media screen and (min-width: ${h1.viewportMin}px) {
+        font-size: calc(${responsiveTextFormula(h1)});
     }
     /* Font-Size: Maximum */
-    @media screen and (min-width: ${headingSize.viewportMax}px) {
-        font-size: ${headingSize.sizeMax}px;
+    @media screen and (min-width: ${h1.viewportMax}px) {
+        font-size: ${h1.sizeMax}px;
     }
     
     font-family: 'Raleway', sans-serif;
