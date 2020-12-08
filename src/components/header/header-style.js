f3244d1ae846bcefbd23108ff37c14c6d1e87768
Sakiskid <tylerdrew.home@gmail.com>
2020-12-08 13:25:22 -0600

    cleaned up header.js by moving style into header-style.js

---
+++ src/components/header/header-style.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const LinkStyleFunc = ({ className, children, to, active }) => (
+    <Link className={className} to={to} active={active}>
+        {children}
+    </Link>
+);
+
+export const HeaderStyled = styled.header`
+    font-family: 'Montserrat', sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 5vh;
+    text-align: center;
+
+    background-color: ${props => props.theme.primary};
+    box-shadow: 0px 2px 2px ${props => props.theme.black};
+`;
+
+export const LinkStyled = styled(LinkStyleFunc)`
+    height: 100%;
+    line-height: 250%;
+    vertical-align: middle;
+    padding: 0px 1%;
+    margin: 0px 0.5%;
+    font-size: 1em;
+    text-decoration: none;
+    border-bottom: 3px solid transparent;
+    color: ${props => props.theme.white};
+
+    /* Set Active */
+    &[active="true"] {
+        color: ${props => props.theme.primarylight};
+        border-bottom-color: ${props => props.theme.primarylight};
+    }
+    /* Style */
+    :hover {
+        color: ${props => props.theme.primarylight};
+        border-bottom: 3px solid;
+    }
+`;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,48 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-// import './header.css';
-
-const LinkStyleFunc = ({ className, children, to, active }) => (
-    <Link className={className} to={to} active={active}>
-        {children}
-    </Link>
-);
-
-const HeaderStyled = styled.header`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 5vh;
-    text-align: center;
-
-    background-color: ${props => props.theme.primary};
-    box-shadow: 0px 2px 2px ${props => props.theme.black};
-`;
-
-const LinkStyled = styled(LinkStyleFunc)`
-    height: 100%;
-    line-height: 250%;
-    vertical-align: middle;
-    padding: 0px 1%;
-    margin: 0px 0.5%;
-    font-size: 1em;
-    text-decoration: none;
-    border-bottom: 3px solid transparent;
-    color: ${props => props.theme.white};
-
-    /* Set Active */
-    &[active="true"] {
-        color: ${props => props.theme.primarylight};
-        border-bottom-color: ${props => props.theme.primarylight};
-    }
-    /* Style */
-    :hover {
-        color: ${props => props.theme.primarylight};
-        border-bottom: 3px solid;
-    }
-`;
+import { HeaderStyled, LinkStyled } from './header-style';
 
 function Header (props) {
     return (
