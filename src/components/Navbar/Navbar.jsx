5779fe4eb4c13256ca149f8807af1a59b7bb41fd
Sakiskid <tylerdrew.home@gmail.com>
2021-01-23 14:37:48 -0600

    feat: added basic responsive navbar separator

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import './navbar.scss';
 import projectdata from 'data/projectdata';
 import NavbarGroup from './NavbarGroup';
+import NavbarSeparator from "./NavbarSeparator";
 
 function Navbar () {
     const location = useLocation();
@@ -12,13 +13,20 @@ function Navbar () {
             {/* Projects Page */}
             <NavbarGroup to="/portfolio" name="Portfolio">
                 {/* Project Links */}
-                <ul>
-                    {projectdata.map(p => <li><Link className='link link-h2'>{p.title}</Link></li>)}
-                </ul>
+                {/* <ul> */}
+                    {projectdata.map(p => <Link className='link link-h2'>{p.title}</Link>)}
+                {/* </ul> */}
             </NavbarGroup>
+
+            <NavbarSeparator />
 
             {/* Gallery */}
             <NavbarGroup to='/gallery' name="Gallery"></NavbarGroup>
+
+            <NavbarSeparator />
+
+            {/* Contact */}
+            <NavbarGroup to='/contact' name='Contact'></NavbarGroup>
         </nav>
     );
 };
---
+++ src/components/Navbar/NavbarSeparator/NavbarSeparator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarSeparator = () => {
+    return (
+        <div className="nav-divider-wrapper">
+            <div className="nav-divider"></div>
+        </div>
+    );
+};
+
+export default NavbarSeparator;
