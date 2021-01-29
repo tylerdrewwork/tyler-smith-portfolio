f51345fb94dbe0e3bfbd13992af9ece881e1af38
Sakiskid <tylerdrew.home@gmail.com>
2021-01-29 14:26:26 -0600

    feat: home title is done with animation, has overlay animation and some nice styling. niiiice.

--- src/components/FXText/FXText.jsx
+++ src/components/FXText/FXText.jsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './FXText.scss';
 
+import _ from 'underscore';
+import { gsap } from "gsap";
+
 const FXText = (props) => {
+    let overlayEls;
+    
+    function startAnimations () {
+        const minDuration = 30;
+        const maxDuration = 60;
+
+        // Animate each overlay separately!
+        for (let i = 0; i < overlayEls.length; i++) {
+            
+            gsap.to(overlayEls[i],
+                {   // Overlay Animation Properties
+                    backgroundPositionX: '1000px', 
+                    yoyo: false,
+                    ease: "linear",
+                    duration: _.random(minDuration, maxDuration),
+                    repeat: -1,
+                })
+            }
+        }
+        
+    // useEffect triggers once on page load, which triggers the animations
+    useEffect (() => {
+        overlayEls = document.querySelectorAll(".fxtext__overlay")
+        startAnimations();
+    }, []);
+
+    // This function renders multiple overlays
+    function renderOverlays () {
+        let layerCount;
+        let renderedOverlays = [];
+        // If no props.layers, then set count to 1 
+        if (!props.layers) { layerCount = 1; }
+        else { layerCount = props.layers; }
+
+        for (let i = 0; i < layerCount; i++) {
+            renderedOverlays.push(
+                <div className={`fxtext__overlay ${props.className} ${props.overlayClass}`}>
+                    {props.children}
+                </div>
+            )
+        }
+
+        return renderedOverlays;
+    }
+
     return (
         <div className={"fxtext"}>
-            {/* The actual text */}
-            <div className={"fxtext__text " + props.className}>
+            {/* The actual text, using template literals for classes */}
+            <div className={`fxtext__text ${props.className} ${props.textClass}`}>
                 {props.children}
             </div>
             {/* The fx overlay */}
-            <div className={"fxtext__overlay " + props.className}>
-                {props.children}
-            </div>
+            {renderOverlays()}
         </div>
     );
 };
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,29 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './home.scss';
 // Gsap Imports
-import { gsap } from "gsap";
 import FXText from 'components/FXText';
 
 function Home () {
-    function startAnimations () {
-        gsap.to(".fxtext__overlay",
-            {backgroundPositionX: '100px', repeat: -1, yoyo: true, duration: 15})
-        }
-        
-    // useEffect triggers once on page load, which triggers the animations
-    useEffect (() => {
-        startAnimations();
-    }, []);
+    
     
     return (
         <div id="home-title-container">
             <FXText className="jumbo home-title">
                 TYLER
             </FXText>
+            <FXText className="jumbo home-title">
+                SMITH
+            </FXText>
             {/* <h1 className="jumbo home-title" id="home-title-first"> */}
                 {/* <div className="home-title__fx">TYLER</div>     */}
             {/* </h1> */}
-            <h1 className="jumbo home-title" id="home-title-last">SMITH</h1>
+            {/* <h1 className="jumbo home-title" id="home-title-last">SMITH</h1> */}
             {/* <SectionHeading>Projects</SectionHeading> */}
             {/* <ProjectWrapper /> */}
         </div>
