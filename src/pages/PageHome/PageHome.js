dbce909b90b099943feeefa0bfc186e8c2f7754c
Sakiskid <tylerdrew.home@gmail.com>
2021-07-18 16:05:04 -0500

    ref: removed sections

--- src/pages/PageHome/PageHome.js
+++ src/pages/PageHome/PageHome.js
@@ -1,14 +1,35 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import './PageHome.scss';
-import HomeAboutSection from './sections/HomeAboutSection';
-import HomeProjectsSection from './sections/HomeProjectsSection';
+import ProjectsGallery from 'components/ProjectsGallery/ProjectsGallery';
 // Gsap Imports\
 
 function Home () {
     return (
         <>
-        <HomeAboutSection/>
-        <HomeProjectsSection/>
+        <section>
+            <h1>
+                About Me
+            </h1>   
+            
+            <p>
+                I'm a C# Unity Game Developer who participated in too many game jams and started making websites. I have Full-Stack MERN, C# Unity Editor, and audio production experience.
+            </p>
+        </section>
+        
+        <section>
+            
+            <h1>
+                Featured Projects
+            </h1> 
+
+            <p>
+                Projects in both Game and Web Development that I'm proud of.
+            </p>
+
+            <ProjectsGallery/>
+
+        </section>
         </>
     )
 }
