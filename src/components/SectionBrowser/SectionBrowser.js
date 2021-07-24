7f9f26d86195c1163922ea71c476f0b668efb716
Sakiskid <tylerdrew.home@gmail.com>
2021-07-24 11:31:02 -0500

    feat: scrollspy implemented

--- src/components/SectionBrowser/SectionBrowser.js
+++ src/components/SectionBrowser/SectionBrowser.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Scrollspy from 'react-scrollspy';
 import "./SectionBrowser.scss";
 
 function SectionBrowser (props) {
     return (
         <div className="sectionbrowser" sections={props.sections}>
-            <nav className="sectionbrowser-header">{RenderSectionHeader(props.sections)}</nav>
+
+            {/* Navigation Header */}
+            <Scrollspy className="sectionbrowser-header" items={GetItemsForScrollspy(props.sections)} currentClassName="current">
+                {RenderSectionHeader(props.sections)}
+            </Scrollspy>
+
+            {/* Navigation Content */}
             <div className="sectionbrowser-content">
                 {RenderSections(props.sections)}
             </div>
+
         </div>
     );
 }
@@ -15,9 +23,11 @@ function SectionBrowser (props) {
 function RenderSectionHeader (sections) {
     return sections.map(section => {
         return (
-            <a key={"header-" + section.title} href={"#sectionbrowser-" + section.title}>
-                {section.title}
-            </a>
+            <li key={"header-" + section.title}>
+                <a href={"#sectionbrowser-" + section.title}>
+                    {section.title}
+                </a>
+            </li>
         )
     })
 }
@@ -31,6 +41,17 @@ function RenderSections (sections) {
             </div>
         )
     })
+}
+
+/// This function gets the ids for the Scrollspy to use
+function GetItemsForScrollspy (sections) {
+    let items = [];
+
+    for (let i = 0; i < sections.length; i++) {
+        items.push("sectionbrowser-" + sections[i].title);
+    }
+
+    return items;
 }
 
 export default SectionBrowser;
