189e447aae37d2e4eac5f737e245e532b57406e9
Sakiskid <tylerdrew.home@gmail.com>
2021-07-30 19:14:58 -0500

    feat: footer works in mobile, fixes to header border radius

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -35,7 +35,7 @@ function Footer () {
                 </p>
             </div>
             <div id="footer-attributions">
-                <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
             </div>
         </footer>
     )
--- src/pages/PageHome/Sections/AboutMe.js
+++ src/pages/PageHome/Sections/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function AboutMe () {
     return (
-        <div>
+        <div className="browser-padding">
             <h2>
                 About Me
                 <hr/>
